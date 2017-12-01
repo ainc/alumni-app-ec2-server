37477460db8f14f0617132c5c423630973d8eb6a
Nick Sladic <nick.sladic@g.uky.edu>
2017-12-01 17:47:59 -0500

    ecosystem config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,23 +3,29 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps: [{
-    name: 'alumni-app-ec2-server',
-    script: './server.js'
-  }],
+  apps : [
+
+    // First application
+    {
+      name      : 'Alumni-API',
+      script    : './server.js'
+    }
+  ],
+
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy: {
-    production: {
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-18-221-88-190.us-east-2.compute.amazonaws.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:ainc/alumni-app-ec2-server.git',
       key  : '~/.ssh/alumni-app-key.pem',
-      user: 'ubuntu',
-      host: 'ec2-18-221-88-190.us-east-2.compute.amazonaws.com',
-      ref: 'origin/master',
-      repo: 'git@github.com:ainc/alumni-app-ec2-server.git',
-      path: '/home/ubuntu/alumni-app-ec2-server',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      path : '/home/ubuntu/',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
+
   }
-}
+};
